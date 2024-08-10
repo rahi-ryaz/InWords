f71ad447d0e5d6886cc258c46d214806371ad570
Rahi Reyaz <rahiryazdar@gmail.com>
2024-08-11 03:19:47 +0530

    initialised handlers,DB,schema and migrated db

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,41 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+// Create the main Hono app
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL:string;
+	}
+}>();
+
+app.post('/api/v1/signup', (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+	return c.text('signup route')
 })
 
-export default app
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+export default app;
